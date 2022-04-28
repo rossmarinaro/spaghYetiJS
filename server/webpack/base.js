cdb66f5dfa03cc581b0777b9818c2162832401e3
Ross <44373734+rossmarinaro@users.noreply.github.com>
2022-04-27 20:18:04 -0400

    Add files via upload

---
+++ server/webpack/base.js
@@ -0,0 +1,76 @@
+
+////http server
+  const port = process.env.PORT || 8080;
+
+/////////////////////////////////////////////// webpack
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+module.exports = {
+  mode: 'development',
+  devtool: 'eval-source-map',
+  entry:  './public/bundle.min.js.js',
+  module: {
+   // noParse: ["ws"],
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: [/\.vert$/, /\.frag$/],
+        use: 'raw-loader'
+      },
+      {
+        test: /\.(gif|png|ogg|jpe?g|svg|xml|mp3|woff)$/i, 
+        use: 'file-loader' ,
+        exclude: [
+          path.resolve(__dirname, '../src/assets/', '../src/')
+        ]
+      },
+      {
+        test: /\.css$/i, 
+        use: 
+           ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  devServer: {
+    port: port,
+    proxy: { 
+      '../socket.io.min.js': { 
+        target: port, 
+        ws: true 
+      } 
+    },
+  },
+  plugins: [
+    new CleanWebpackPlugin({
+      root: path.resolve(__dirname, '../')
+    }),
+    new webpack.DefinePlugin({
+      CANVAS_RENDERER: JSON.stringify(true),
+      WEBGL_RENDERER: JSON.stringify(true)
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    }),
+  ]
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ server/webpack/prod.js
@@ -0,0 +1,32 @@
+const merge = require("webpack-merge");
+const path = require("path");
+const base = require("./base");
+const TerserPlugin = require("terser-webpack-plugin");
+//const CopyWebpackPlugin = require('copy-webpack-plugin');
+module.exports = merge(base, {
+  mode: "production",
+  output: {
+    filename: "bundle.min.js"
+  },
+  devtool: false,
+  performance: {
+    maxEntrypointSize: 900000,
+    maxAssetSize: 900000
+  },
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          output: {
+            comments: false
+          }
+        }
+      })
+    ]
+  },
+  plugins: [
+    // new CopyWebpackPlugin({
+    //  patterns: [{ from: 'src/assets/meatballin', to: 'assets' }]
+    // })
+  ]
+});
