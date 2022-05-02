a6ca595ab520c90e1d2598eba7df0667b04f2058
Ross <44373734+rossmarinaro@users.noreply.github.com>
2022-05-02 14:40:12 -0400

    Delete api.js

--- server/api.js
+++
@@ -1,9 +0,0 @@
-/* API */
-
-
-const express = require('express'),
-	App = express()
-    server = require('http').Server(App), 
-    port = process.env.PORT || 9090;
-
-module.exports = { express, App, server, port}
