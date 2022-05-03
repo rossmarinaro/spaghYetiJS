c3153c2aa633c5560f1e89af01dcca856767ed97
Ross <44373734+rossmarinaro@users.noreply.github.com>
2022-05-03 15:55:36 -0400

    Add files via upload

---
+++ server/extern/main.py
@@ -0,0 +1,34 @@
+# start server, open browser
+
+#import os
+import webbrowser
+from http.server import HTTPServer, CGIHTTPRequestHandler
+
+
+#os.chdir('C:\project data\projects\spagYeti_game_engine\server\dist')
+
+""" serve on port """
+
+PORT = 1776
+
+
+server_object = HTTPServer(server_address=('', PORT), RequestHandlerClass=CGIHTTPRequestHandler)
+
+print("python serving at port", PORT)
+
+
+""" open browser """
+
+webbrowser.open("http://localhost:{}/dist".format(PORT))
+
+
+server_object.serve_forever()
+
+
+
+
+
+
+
+
+
