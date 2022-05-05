3e6a9c59dbc05b923c70fe52a100e1d1e1f1e0c1
Ross <44373734+rossmarinaro@users.noreply.github.com>
2022-05-05 15:22:07 -0400

    Update webpack.js

--- server/app/webpack.js
+++ server/app/webpack.js
@@ -32,83 +32,121 @@ class WebpackConfig {
 
             `
 
-            const TerserPlugin = require("terser-webpack-plugin");
-            const webpack = require('webpack');
-            const HtmlWebpackPlugin = require('html-webpack-plugin');
-            const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+                const webpack = require('webpack'),
+                TerserPlugin = require("terser-webpack-plugin"),
+                HtmlWebpackPlugin = require('html-webpack-plugin');
 
     
-            module.exports = {
-                entry: {
-                    main: "/project/game.js",
-                },
-                output: {
-                    path: "${filepath}",
-                    filename: 'bundle.min.js'
-                },
+                module.exports = {
+                    entry: {
+                        main: "/project/game.js",
+                    },
+                    output: {
+                        path: "${filepath}",
+                        filename: 'bundle.min.js'
+                    },
+                    
+                    // module: {
+                    //     rules: [
+                    //         // {
+                    //         // test: /\.js$/,
+                    //         // exclude: /node_modules/,
+                    //         // use: {
+                    //         //     loader: 'babel-loader'
+                    //         // }
+                    //         // },
+                    //         // {
+                    //         // test: [/\.vert$/, /\.frag$/],
+                    //         // use: 'raw-loader'
+                    //         // },
+                    //         // {
+                    //         // test: /\.(gif|png|ogg|jpe?g|svg|xml|mp3|woff)$/i, 
+                    //         // use: 'file-loader' 
+                    //         // },
+                    //         // {
+                    //         // test: /\.css$/i, 
+                    //         // use: 
+                    //         //     ['style-loader', 'css-loader']
+                    //         // }
+                    //     ]
+                    // },
+
+                    performance: {
+                        maxEntrypointSize: 900000,
+                        maxAssetSize: 900000
+                    },
+                    optimization: {
+                        minimizer: [
+                        new TerserPlugin({
+                            terserOptions: {
+                            output: {
+                                comments: false
+                            }
+                            }
+                        })
+                        ]
+                    },
+
+                    plugins: [
                 
-                module: {
-                    rules: [
-                        {
-                        test: /\.js$/,
-                        exclude: /node_modules/,
-                        use: {
-                            loader: 'babel-loader'
-                        }
-                        },
-                        {
-                        test: [/\.vert$/, /\.frag$/],
-                        use: 'raw-loader'
-                        },
-                        {
-                        test: /\.(gif|png|ogg|jpe?g|svg|xml|mp3|woff)$/i, 
-                        use: 'file-loader' 
-                        },
-                        {
-                        test: /\.css$/i, 
-                        use: 
-                            ['style-loader', 'css-loader']
-                        }
+                        new webpack.DefinePlugin({
+                            CANVAS_RENDERER: JSON.stringify(true),
+                            WEBGL_RENDERER: JSON.stringify(true)
+                        }),
+                        new HtmlWebpackPlugin({
+                            template: "./project/index.html"
+                        }),
                     ]
-                },
-
-                performance: {
-                    maxEntrypointSize: 900000,
-                    maxAssetSize: 900000
-                },
-                optimization: {
-                    minimizer: [
-                    new TerserPlugin({
-                        terserOptions: {
-                        output: {
-                            comments: false
-                        }
-                        }
-                    })
-                    ]
-                },
-
-                plugins: [
-            
-                    new webpack.DefinePlugin({
-                        CANVAS_RENDERER: JSON.stringify(true),
-                        WEBGL_RENDERER: JSON.stringify(true)
-                    }),
-                    new HtmlWebpackPlugin({
-                        template: "./project/index.html"
-                    }),
-                ]
-            };
+                };
 
             `
         );
 
+
+        // return (     WORKS
+        //     `
+        //     module.exports = {
+        //         entry: {
+        //             main: "/project/game.js",
+        //         },
+        //         output: {
+        //             path: "/dist",
+        //             filename: 'bundle.min.js'
+        //         }
+                
+        //     };
+        //     `
+        // );
+
+        // return (
+        //     `
+        //     module.exports = {
+        //         entry: {
+        //             main: "/project/game.js",
+        //         },
+        //         output: {
+        //             path: "C:/Users/Ross/AppData/Local/Programs/spaghyetijs/resources/buildSys/dist",
+        //             filename: 'bundle.min.js'
+        //         }
+                
+        //     };
+        //     `
+        // );
     }
 
 }
 
 
 module.exports = { WebpackConfig };
+
+
+
+
+
+
+
+
+
 
 
 
